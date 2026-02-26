129eb987a0a87020226d5bad0bdfbc0353544513
Kateryna Troyan <ket.tro@gmail.com>
2026-02-26 14:51:00 +0200

    Fix: hero image right-anchored, remove scale transform, no white strips

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,49 +6,40 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
       <Navbar />
-      <img
-        src="/assets/images/Foto_hero.jpg"
-        alt="Art that speaks for nature"
-        className={styles.bgImage}
-      />
 
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          ART THAT SPEAKS
-          <br />
-          FOR NATURE
-        </h1>
-        <p className={styles.subtitle}>
-          We draw attention to ecology
-          <br />
-          and animal protection
-          <br />
-          through creativity
-          <br />
-          and art installations
-        </p>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            ART THAT SPEAKS
+            <br />
+            FOR NATURE
+          </h1>
+          <p className={styles.subtitle}>
+            We draw attention to ecology
+            <br />
+            and animal protection
+            <br />
+            through creativity
+            <br />
+            and art installations
+          </p>
+        </div>
 
         <div className={styles.buttons}>
           <Link to="/events" className={styles.btnOutline}>
             Events{" "}
-            <svg className={styles.icon}>
+            <svg className={styles.iconBtnOutline}>
               <use href="/assets/sprite.svg#Arrow up-right" />
             </svg>
           </Link>
           <Link to="/apply" className={styles.btnFilled}>
             Apply Now{" "}
-            <svg className={styles.icon}>
+            <svg className={styles.iconBtnFilled}>
               <use href="/assets/sprite.svg#Arrow up-right" />
             </svg>
           </Link>
         </div>
       </div>
-
-      <blockquote className={styles.quote}>
-        To hear nature
-        <br />
-        is to change the world
-      </blockquote>
     </section>
   );
 };
